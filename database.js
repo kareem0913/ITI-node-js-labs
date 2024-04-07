fd42708e09738c62c8ac0bc35529d6f043d8937d
Kareem Zaher <kareem.345@yahoo.com>
2024-04-07 17:52:20 +0200

    update datebase file

--- database.js
+++ database.js
@@ -1,26 +1,3 @@
-// const { MongoClient } = require("mongodb");
-
-// async function main() {
-//   try {
-//     const client = new MongoClient(
-//       "mongodb+srv://kareem345:ig0iFjpXD9NnTCGp@cluster0.llvl7n3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-//     );
-//     await client.connect();
-//     const db = client.db("sample_mflix");
-//     const collection = db.collection("users");
-//     return collection;
-//   } catch (error) {
-//     throw error;
-//   }
-//   //   await collection.insertOne({
-//   //     id: 123,
-//   //     createdBy: "kareem",
-//   //   });
-//   //   const randomMovies = await collection.findOne({ createdBy: "kareem" });
-// }
-
-// module.exports = { main };
-
 const { MongoClient } = require("mongodb");
 
 async function main() {
