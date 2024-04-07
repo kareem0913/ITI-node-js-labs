94f78f3598429c3959a23c52e41fdf9d10faea79
Kareem Zaher <kareem.345@yahoo.com>
2024-04-07 17:50:17 +0200

    node js , mongodb crud operation

--- server.js
+++ server.js
@@ -1,39 +1,73 @@
-const fs = require("fs");
-const path = require("path");
+const express = require("express");
+const http = require("http");
+const { main } = require("./database");
+const { Collection } = require("mongodb");
+const app = express();
 
-const { program } = require("commander");
+const server = http.createServer(app);
+app.use(express.json()); // this is built in middleware in express to convert request to json data
 
-program
-  .option("--dir-path, --dir <dir>")
-  .option("--text <text>")
-  .option("--allow-ext, --extenstion <extenstion>")
-  .parse(process.argv);
-
-const options = program.opts();
-
-fs.readdir(options.dir, (err, files) => {
-  files.forEach((file) => {
-    const filePath = path.join(options.dir, file);
-    fs.stat(filePath, (err, stats) => {
-      if (stats.isDirectory()) {
-        fs.readdir(filePath, (err, subfiles) => {
-          subfiles.forEach((subfile) => {
-            const subFilePath = path.join(filePath, subfile);
-            const fileExtenstion = path.extname(subFilePath);
-            if (fileExtenstion == options.extenstion) {
-              fs.writeFile(subFilePath, options.text, (err) => {
-                console.log(err);
-              });
-            }
-          });
-        });
-      }
-    });
-    const fileExtenstion = path.extname(filePath);
-    if (fileExtenstion == options.extenstion) {
-      fs.writeFile(filePath, options.text, (errr) => {
-        console.log(err);
-      });
-    }
-  });
+// routes
+app.get("/get/:id", async (req, res) => {
+  try {
+    const userId = Number(req.params.id);
+    const client = await main();
+    const db = client.db("sample_mflix");
+    const collection = db.collection("users");
+    const userData = await collection.findOne({ id: userId });
+    client.close();
+    res.status(200).json({ user: userData });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to connect to the database" });
+  }
 });
+
+app.post("/addUser", async (req, res) => {
+  try {
+    const client = await main();
+    const db = client.db("sample_mflix");
+    const collection = db.collection("users");
+    await collection.insertOne(req.body);
+    client.close();
+    res.status(201).json({ success: "data is added successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to connect to the database" });
+  }
+});
+
+app.put("/edit/:id", async (req, res) => {
+  try {
+    const userId = Number(req.params.id);
+    const client = await main();
+    const db = client.db("sample_mflix");
+    const collection = db.collection("users");
+    await collection.updateOne({ id: userId }, { $set: req.body });
+    res.status(201).json({ success: "data is added successfully  " + userId });
+    client.close();
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  try {
+    const userId = Number(req.params.id);
+    const client = await main();
+    const db = client.db("sample_mflix");
+    const collection = db.collection("users");
+    await collection.deleteOne({ id: userId });
+    client.close();
+    res.status(200).json({ success: "user is successfully deleted" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to connect to the database" });
+  }
+});
+
+const PORT = 3000;
+async function startServer() {
+  await main();
+  server.listen(PORT, () => {
+    console.log(`server is listen on prot ${PORT}`);
+  });
+}
+startServer();
