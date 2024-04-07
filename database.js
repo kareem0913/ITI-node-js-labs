5b91ea7da5c1086a124c93aded96459d7743be69
Kareem Zaher <kareem.345@yahoo.com>
2024-04-07 17:54:03 +0200

    update database file

--- database.js
+++ database.js
@@ -1,9 +1,8 @@
 const { MongoClient } = require("mongodb");
 
 async function main() {
-  const client = new MongoClient(
-    "mongodb+srv://kareem345:ig0iFjpXD9NnTCGp@cluster0.llvl7n3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  );
+  const client = new MongoClient();
+  // url
   try {
     await client.connect();
     return await client;
